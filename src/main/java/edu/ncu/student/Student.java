94186d824112f5dfbc148c6a9b5c39f0ba4ffa3a
Yang-Lin <fwautotechtw@gmail.com>
2022-05-15 22:41:32 +0800

    tutorial_5_1 switch ... case

--- src/main/java/edu/ncu/student/Student.java
+++ src/main/java/edu/ncu/student/Student.java
@@ -16,7 +16,92 @@ public class Student {
     }
     /*Method*/
     public void print(){
-        System.out.println(name + "\tenglish:" + english + "\tmath:" + math + "\taverage:" + ((english+math)/2));
+        float average = getAverage();
+        System.out.println(name + "\tenglish:" + english +
+                "\tmath:" + math +
+                "\taverage:" + getAverage() +
+                "\tPass/Fail:" + ((average >= 60) ? "PASS":"FAIL"));
+        /* ==" ==" =="*/
+        //  90 to 100   -> grading is A
+        //  80 to 89    -> grading is B
+        //  70 to 79    -> grading is C
+        //  60 to 69    -> grading is D
+        //  59 down     -> grading is F
+        char grading = 'F';
+        switch ((int) (average/10)){
+            case 10:
+            case 9:
+                grading = 'A';
+                break;
+            case 8:
+                grading = 'B';
+                break;
+            case 7:
+                grading = 'C';
+                break;
+            case 6:
+                grading = 'D';
+                break;
+            default:
+                grading = 'F';
+        }
+        //[Case:1]
+        /*if(average <=100 && average >= 90){
+            System.out.println("\tGrading is:" + "A");
+        }else{
+            if(average <= 89 && average >= 80){
+                System.out.println("\tGrading is:" + "B");
+            }else {
+                if(average <= 79 && average >= 70){
+                    System.out.println("\tGrading is:" + "C");
+                }else{
+                    if(average <= 69 && average >= 60){
+                        System.out.println("\tGrading is:" + "D");
+                    }else {
+                        System.out.println("\tGrading is:" + "F");
+                    }
+                }
+            }
+        }*/
+        //[Case:2]
+        /*if(average <= 100 && average >= 90){
+            System.out.println("A");
+        }
+        else if(average <= 89 && average >= 80){
+            System.out.println("B");
+        }
+        else if(average <= 79 && average >= 70){
+            System.out.println("C");
+        }
+        else if(average <= 69 && average >= 60){
+            System.out.println("D");
+        }
+        else if(average <= 59){
+            System.out.println("F");
+        }*/
+        //[case:3]
+        /*char grading = 'F';
+        if(average <= 100 && average >= 90){
+            grading = 'A';
+        }
+        else if(average <= 89 && average >= 80){
+            grading = 'B';
+        }
+        else if(average <= 79 && average >= 70){
+            grading = 'C';
+        }
+        else if(average <= 69 && average >= 60){
+            grading = 'D';
+        }
+        else if(average <= 59){
+            grading = 'F';
+        }*/
+        System.out.println("\tGrading is :" + grading);
+        /*if (getAverage() >= 60){
+            System.out.println("\tPASS");
+        }else {
+            System.out.println("\tFAIL");
+        }*/
     }
     public float highest(){
 //        float max = (english>math)? english:math;
@@ -25,6 +110,10 @@ public class Student {
         }else {
             max=math;
         }*/
-        return (english > math) ? english:math;
+        return ((english > math) ? english:math);
+    }
+
+    public float getAverage(){
+        return (english+math)/2;
     }
 }
