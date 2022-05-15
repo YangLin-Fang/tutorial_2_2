070e919e5370df9329552413a024c3650a5cee3d
Yang-Lin <fwautotechtw@gmail.com>
2022-05-16 01:09:46 +0800

    tutorial_5_5 Unit Test(ok)

--- src/test/java/edu/ncu/PersonTest.java
+++ src/test/java/edu/ncu/PersonTest.java
@@ -2,13 +2,14 @@ package edu.ncu;
 
 import edu.ncu.Person;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class PersonTest {
 
     @Test
     public void bmiTest(){
         Person person = new Person(66.5f,1.7f);
-        Assert.assertEquals(66.5f/(1.7f*1.7f), person.bmi());
+        Assertions.assertEquals(66.5f/(1.7f*1.7f), person.bmi());
     }
 }
--- src/test/java/edu/ncu/StudentTest.java
+++ src/test/java/edu/ncu/StudentTest.java
@@ -2,14 +2,15 @@ package edu.ncu;
 
 import edu.ncu.student.Student;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StudentTest {
 
     @Test
     void averageTest(){
         Student student = new Student("YL",60,80);
-        Assert.assertEquals((60+80)/2, student.getAverage());
+        Assertions.assertEquals(((60+80)/2), student.getAverage());
 
     }
 }
