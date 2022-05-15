5fb64e107f06c70e4c9491c870b0bd3743124ac9
Yang-Lin <fwautotechtw@gmail.com>
2022-05-15 19:34:00 +0800

    tutorial_4_1 活用 Java 原生的類別庫。

--- src/main/java/edu/ncu/student/StudentRunner.java
+++ src/main/java/edu/ncu/student/StudentRunner.java
@@ -1,8 +1,17 @@
 package edu.ncu.student;
 
+import java.util.Scanner;
+
 public class StudentRunner {
     public static void main(String[] args) {
-        Student stu = new Student("Teddy",78,77);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter student's name:");
+        String name = scanner.next();
+        System.out.println("Please enter student's english:");
+        float english = scanner.nextFloat();
+        System.out.println("Please enter student's math:");
+        float math = scanner.nextFloat();
+        Student stu = new Student(name,english,math);
         stu.print();
     }
 }
