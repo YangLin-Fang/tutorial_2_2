017df2f55ad37ff82b77de21a330457eedfe1f0b
Yang-Lin <fwautotechtw@gmail.com>
2022-05-15 16:42:47 +0800

    tutorial_3_2 建構子練習

--- src/main/java/edu/ncu/Hello.java
+++ src/main/java/edu/ncu/Hello.java
@@ -5,7 +5,9 @@ public class Hello {
 //        System.out.println("Hello Java");
 //        new Person();   //在記憶體中將類別生出來，被生出來的這個東西被稱之為物件。
 //        new Person().hello();   //hello()是Person()物件裡的方法
-        Person p = new Person();    //為了重複使用，去產生一個 p 物件來儲存 Person() 物件。
+//        Person p = new Person(79.08f,1.74f);    //為了重複使用，去產生一個 p 物件來儲存 Person() 物件。
+//        Person p = new Person();
+        Person p = new Person("Teddy",79.08f,174.0f);
         p.hello();
 //--------------------- tutorial_2_4 --------------------------
 //        int a = 19;
--- src/main/java/edu/ncu/Person.java
+++ src/main/java/edu/ncu/Person.java
@@ -1,10 +1,26 @@
 package edu.ncu;
 
 public class Person {
+    /*Field/Property*/
     String name;
     float weight;
     float height;
 
+    /*Constructor*/
+    public Person(){}
+
+    public Person(float weight, float height){
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public Person(String name, float weight, float height){
+        this(weight, height);
+        this.name = name;
+//        this.weight = weight;
+//        this.height = height;
+    }
+    /*Method*/
     public float bmi(){
         return weight/(height*height);
     }
